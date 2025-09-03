b1b7e02e2340ef8db37409a2dea32a3549dc319b
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-09-03 00:52:34 -0400

    Experiment 12 Program 09.cpp

---
+++ Experiment 12 Program 09.cpp
@@ -0,0 +1,31 @@
+//Operator Overrulled
+#include<iostream>
+using namespace std;
+class Complex
+{
+    private:
+    int real,imag;
+    public:
+    Complex(int r=0, int i=0)
+    {
+        real=r;
+        imag=i;
+    }
+    Complex operator+(Complex const&obj)
+    {
+        Complex res;
+        res.real=real+obj.real;
+        res.imag=imag+obj.imag;
+        return res;
+    }
+    void print()
+    {
+        cout<<real<<"+i"<<imag<<'\n';
+    }
+};
+int main()
+{
+    Complex c1(11,2),c2(2,5);
+    Complex c3= c1+c2;
+    c3.print();
+}
